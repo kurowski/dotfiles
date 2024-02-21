0b4330475764581a0fd1b9e077e4424884419618
Brandt Kurowski <bkurowski@uceap.universityofcalifornia.edu>
2024-02-21 13:46:10 -0500

    configure atuin

--- install.sh
+++ install.sh
@@ -6,6 +6,8 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 ln -s $DOTFILES/.zshrc ~
 [[ ! -f ~/.p10k.zsh ]] || mv ~/.p10k.zsh ~/.p10k.zsh.orig
 ln -s $DOTFILES/.p10k.zsh ~
+[[ ! -f ~/.config/atuin/config.toml ]] || mv ~/.config/atuin/config.toml ~/.config/atuin/config.toml.orig
+ln -s $DOTFILES/atuin/config.toml ~/.config/atuin
 if command -v defaults &> /dev/null; then
   defaults write com.apple.Safari UserStyleSheetLocationURLString -string "$DOTFILES/Safari/stylesheet.css"
 fi
