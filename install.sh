c7e675cf4e3aa7c4e33a1619e88a716f50dcc886
Brandt Kurowski <bkurowski@uceap.universityofcalifornia.edu>
2024-02-22 16:13:49 -0500

    fix for fresh installs

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@ ln -s $DOTFILES/.zshrc ~
 [[ ! -f ~/.p10k.zsh ]] || mv ~/.p10k.zsh ~/.p10k.zsh.orig
 ln -s $DOTFILES/.p10k.zsh ~
 [[ ! -f ~/.config/atuin/config.toml ]] || mv ~/.config/atuin/config.toml ~/.config/atuin/config.toml.orig
+mkdir -p ~/.config/atuin
 ln -s $DOTFILES/atuin/config.toml ~/.config/atuin
 if command -v defaults &> /dev/null; then
   defaults write com.apple.Safari UserStyleSheetLocationURLString -string "$DOTFILES/Safari/stylesheet.css"
