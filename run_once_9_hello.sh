ce0c3d13568a09a8dc5fa0681eb29ab1f59dbf3d
Brandt Kurowski <bkurowski@uceap.universityofcalifornia.edu>
2025-07-07 12:46:33 -0400

    fmt

--- run_once_9_hello.sh
+++ run_once_9_hello.sh
@@ -3,17 +3,11 @@
 glow - << 'EOF'
 # Welcome to your new system
 
-Your dotfiles have been _successfully applied_ 😌
+Your dotfiles have been successfully applied 😌
 
 ## Next Steps
 
-1. Clone all the repos you might be working on:
-    ``` bash
-    .j clone-all-repos
-    ```
-2. **Restart your shell** to load new configurations
-
----
-
-**Happy coding!** 🚀
+1. Clone all the repos you might be working on: `.j clone-all-repos`
+2. _Restart your shell_ to load new configurations
+3. **Happy coding!** 🚀
 EOF
