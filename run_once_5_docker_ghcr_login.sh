41da2df6169121c7e196f1331851c8e8d1351b43
Brandt Kurowski <bkurowski@uceap.universityofcalifornia.edu>
2025-10-03 12:26:48 -0400

    check for docker

--- run_once_5_docker_ghcr_login.sh
+++ run_once_5_docker_ghcr_login.sh
@@ -1,3 +1,5 @@
 #!/bin/zsh
 
-gh auth token | docker login ghcr.io --username $(gh api user --jq .login)$ --password-stdin
+if command -v docker &> /dev/null; then
+  gh auth token | docker login ghcr.io --username $(gh api user --jq .login)$ --password-stdin
+fi
